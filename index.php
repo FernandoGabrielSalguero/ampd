2dfd0f7fcad5580b829d25230467ec6e3a3a60a6
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-05-19 20:16:22 -0300

    main

--- index.php
+++ index.php
@@ -176,6 +176,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="form-group password-container">
                 <label for="contrasena">Contraseña:</label>
                 <input type="password" name="contrasena" id="contrasena" required>
+                <span class="toggle-password">👁️</span>
             </div>
             <div class="form-group">
                 <button type="submit">INGRESAR</button>
