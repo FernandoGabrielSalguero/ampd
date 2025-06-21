99a0634ed195f027ae8e419f21b5356126527071
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-06-21 12:16:49 -0300

    main

--- views/admin/admin_altaUsuarios.php
+++ views/admin/admin_altaUsuarios.php
@@ -111,7 +111,7 @@ $telefono = $_SESSION['telefono'] ?? 'Sin teléfono';
                 <div class="card">
                     <h2>Crear nuevo usuario</h2>
                     <form class="form-modern" id="formUsuario">
-                        <div class="form-grid grid-2">
+                        <div class="form-grid grid-4">
 
                             <!-- Nombre -->
                             <div class="input-group">
