68ab0a25befa3143e35bcc125d0635fc8a29e594
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-06-22 11:24:02 -0300

    main

--- views/admin/admin_importarUsuarios.php
+++ views/admin/admin_importarUsuarios.php
@@ -75,7 +75,7 @@ $telefono = $_SESSION['telefono'] ?? 'Sin teléfono';
                         <span class="material-icons" style="color: #5b21b6;">upload_file</span><span class="link-text">Carga Masiva</span>
                     </li>
                     <li onclick="location.href='admin_pagoFacturas.php'">
-                        <span class="material-icons" style="color: #5b21b6;">upload_file</span><span class="link-text">Pago Facturas</span>
+                        <span class="material-icons" style="color: #5b21b6;">attach_money</span><span class="link-text">Pago Facturas</span>
                     </li>
                     <li onclick="location.href='../../../logout.php'">
                         <span class="material-icons" style="color: red;">logout</span><span class="link-text">Salir</span>
