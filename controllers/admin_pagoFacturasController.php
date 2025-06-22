3aa1b599d1435e9d25f44dd91103d94738fc3491
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-06-22 13:45:56 -0300

    main

--- controllers/admin_pagoFacturasController.php
+++ controllers/admin_pagoFacturasController.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../config.php';
-require_once '../../models/admin_pagoFacturasModel.php';
+require_once __DIR__ . '/../models/admin_pagoFacturasModel.php';
 
 // ================================
 // 🔍 AJAX: Buscar datos por DNI
